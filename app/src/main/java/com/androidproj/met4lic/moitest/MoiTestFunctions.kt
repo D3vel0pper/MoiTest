e08ad2c0768070c246dcc651402db6273f770e5f
D3vel0pper <toamaindex@gmail.com>
2019-06-22 15:23:12 +0900

    add: add test functions and functions for Moi test

---
+++ app/src/main/java/com/androidproj/met4lic/moitest/MoiTestFunctions.kt
@@ -0,0 +1,22 @@
+package com.androidproj.met4lic.moitest
+
+/**
+ * Created by D3vel0pper on 2019/06/22.
+ */
+class MoiTestFunctions {
+    fun countMoi(target: String): Int {
+        return 0
+    }
+
+    fun replaceMoi(target: String): String {
+        return ""
+    }
+
+    fun reverseMoi(target: String): String {
+        return ""
+    }
+
+    fun cutMoi(target: String): String {
+        return ""
+    }
+}
---
+++ app/src/test/java/com/androidproj/met4lic/moitest/TestMoiFunctions.kt
@@ -0,0 +1,45 @@
+package com.androidproj.met4lic.moitest
+
+import org.junit.Assert
+import org.junit.Test
+import com.androidproj.met4lic.moitest.MoiTestFunctions as funcs
+
+/**
+ * Created by D3vel0pper on 2019/06/22.
+ */
+class TestMoiFunctions {
+    @Test
+    fun testCountMoi() {
+        val pattern = "moiABCDmoiEFgmoi"
+        val funcs = funcs()
+        Assert.assertEquals(3, funcs.countMoi(pattern))
+    }
+
+    @Test
+    fun testReplaceMoi() {
+        val pattern = "moiABCDmoiEFgmoi"
+        val expected = "MOIABCDMOIEFgMOI"
+        val funcs = funcs()
+        Assert.assertEquals(expected, funcs.replaceMoi(pattern))
+    }
+
+    @Test
+    fun testReverseMoi() {
+        val pattern = "123moi45moi6789"
+        val expected = "9876moi54moi321"
+        val funcs = funcs()
+        Assert.assertEquals(expected, funcs.replaceMoi(pattern))
+    }
+
+    @Test
+    fun testCutMoi() {
+        var pattern = "moiABCDmoiEFGHIJKLMNmoiOPmoiQRST"
+        var expected = "moiABCDmoiEFGLMNmoiOPmoiQRS"
+        val funcs = funcs()
+        Assert.assertEquals(expected, funcs.replaceMoi(pattern))
+
+        pattern = "ABCDEFG"
+        expected = ""
+        Assert.assertEquals(expected, funcs.cutMoi(pattern))
+    }
+}
